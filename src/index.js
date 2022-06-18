a58af03a7f455ae4e303f86121e2dfac6e59a6a3
anonGTF <anongtf26@gmail.com>
2022-06-18 10:04:29 +0700

    setup material ui, firebase, redux, typescript, router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const firebaseConfig = {
+  apiKey: "AIzaSyCV_MMROnas95RuM20yOCNY8U_lItJ-0F8",
+  authDomain: "peduligizi-71afd.firebaseapp.com",
+  projectId: "peduligizi-71afd",
+  storageBucket: "peduligizi-71afd.appspot.com",
+  messagingSenderId: "613558690983",
+  appId: "1:613558690983:web:8fea936b749fb39227a782",
+  measurementId: "G-T93WL3SETD"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+ReactDOM.render(
+  (<React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>),
+  document.getElementById('root') || document.createElement('div')
 );
 
 // If you want to start measuring performance in your app, pass a function
