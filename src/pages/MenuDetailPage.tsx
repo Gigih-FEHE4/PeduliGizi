61138beca1696449025eb04c69012e74e5e96f09
anonGTF <anongtf26@gmail.com>
2022-06-28 08:59:11 +0700

    wrong file

--- src/pages/MenuDetailPage.tsx
+++ src/pages/MenuDetailPage.tsx
@@ -1,113 +1,57 @@
-import { Container, Grid, Link, Box } from "@mui/material";
-import { Link as RouterLink, useParams } from "react-router-dom";
-import Header from "../Components/Header";
+import Header from '../Components/Header'
+import { Container } from '@mui/material'
+import { useParams } from 'react-router-dom'
 
-const ArticleDetailPage = () => {
-    const dummyArticles = [
-        {
-            id: "ASDDGSDGIDSGJNSGN",
-            title: "Cegah Stunting dengan Perbaikan Pola Makan, Pola Asuh dan Sanitasi",
-            desc: "Sebagian besar masyarakat mungkin belum memahami istilah yang disebut stunting. Stunting adalah masalah kurang gizi kronis yang disebabkan oleh kurangnya asupan gizi dalam waktu yang cukup lama, sehingga mengakibatkan gangguan pertumbuhan pada anak yakni tinggi badan anak lebih rendah atau pendek (kerdil) dari standar usianya....",
-            image: "https://dinkes.pekalongankota.go.id/upload/halaman/halaman_20190328114513.png"
-        },
-        {
-            id: "KJLANSJKLNJKL",
-            title: "Berkenalan dengan Stunting: Penyebab, Dampak, dan Pencegahannya",
-            desc: "Proses tumbuh kembang anak tak hanya berlangsung pesat ketika anak memasuki usia balita.  Seribu hari pertama kehidupan anak justru menjadi momen penting yang memengaruhi tumbuh kembangnya di masa anak-anak hingga beranjak dewasa. Hingga kini, masalah anak stunting masih menjadi momok menakutkan yang menghantui anak-anak di berbagai belahan dunia, termasuk Indonesia....",
-            image: "https://static.republika.co.id/uploads/images/inpicture_slide/ilustrasi_200928222258-299.jpg"
-        },
-        {
-            id: "AWIEJFOWIAGOAWNVOAW",
-            title: "Kebutuhan Nutrisi Gizi Ibu Hamil Yang Harus Dipenuhi",
-            desc: "Asupan gizi ibu hamil menjadi faktor penting baik untuk pemenuhan nutrisi ibu hamil atau pun untuk pertumbuhan dan perkembangan janin dalam kandungan. Bahkan, dapat mengurangi risiko penyakit kronis pada anak Anda di masa mendatang. Yuk, simak daftar asupan gizi ibu hamil yang harus dipenuhi berikut ini.",
-            image: "https://res.cloudinary.com/dk0z4ums3/image/upload/v1626582186/attached_image/inilah-alasan-mengapa-ibu-hamil-mengalami-ngidam.jpg"
-        }
-    ];
-
-    const { id } = useParams<{ id: string }>();
-    return (
-        <>
-            <Header />
-            <Container maxWidth="lg">
-                <h1>Artikel</h1>
-                <h2>Cegah Stunting dengan Perbaikan Pola Makan, Pola Asuh dan Sanitasi</h2>
-                <Grid container spacing={10}>
-                    <Grid item xs={6} md={8}>
-                        <img width={780} alt="article thumbnail" src="https://res.cloudinary.com/dk0z4ums3/image/upload/v1626582186/attached_image/inilah-alasan-mengapa-ibu-hamil-mengalami-ngidam.jpg" />
-                        <p>Sebagian besar masyarakat mungkin belum memahami istilah yang disebut stunting. Stunting adalah masalah kurang gizi kronis yang disebabkan oleh kurangnya asupan gizi dalam waktu yang cukup lama, sehingga mengakibatkan gangguan pertumbuhan pada anak yakni tinggi badan anak lebih rendah atau pendek (kerdil) dari standar usianya. </p>
-                        <p>Kondisi tubuh anak yang pendek seringkali dikatakan sebagai faktor keturunan (genetik) dari kedua orang tuanya, sehingga masyarakat banyak yang hanya menerima tanpa berbuat apa-apa untuk mencegahnya. Padahal seperti kita ketahui, genetika merupakan faktor determinan kesehatan yang paling kecil pengaruhnya bila dibandingkan dengan faktor perilaku, lingkungan (sosial, ekonomi, budaya, politik), dan pelayanan kesehatan. Dengan kata lain, stunting merupakan masalah yang sebenarnya bisa dicegah.</p>
-                        <p>Salah satu fokus pemerintah saat ini adalah pencegahan stunting. Upaya ini bertujuanagar anak-anak Indonesia dapat tumbuh dan berkembang secara optimal dan maksimal, dengan disertai kemampuan emosional, sosial, dan fisik yang siap untuk belajar, serta mampu berinovasi dan berkompetisi di tingkat global.</p>
-                        <p>“Terdapat tiga hal yang harus diperhatikan dalam pencegahan stunting, yaitu perbaikan terhadap pola makan, pola asuh, serta perbaikan sanitasi dan akses air bersih”, tutur Menteri Kesehatan RI, Nila Farid Moeloek, di Jakarta (7/4).</p>
-                        <ol>
-                            <li>
-                                <h3>Pola Makan</h3>
-                                <p>
-                                    Masalah stunting dipengaruhi oleh rendahnya akses terhadap makanan dari segi 
-                                    jumlah dan kualitas gizi, serta seringkali tidak beragam.
-
-                                    Istilah “Isi Piringku” dengan gizi seimbang perlu diperkenalkan dan dibiasakan 
-                                    dalam kehidupan sehari-hari. Bagi anak-anak dalam masa pertumbuhan,  memperbanyak sumber protein sangat dianjurkan, di samping tetap membiasakan 
-                                    mengonsumsi buah dan sayur.
-
-                                    Dalam satu porsi makan, setengah piring diisi oleh sayur dan buah, setengahnya lagi 
-                                    diisi dengan sumber protein (baik nabati maupun hewani) dengan proporsi lebih 
-                                    banyak daripada karbohidrat.
-                                </p>
-                            </li>
-                            <li>
-                                <h3>Pola Asuh</h3>
-                                <p>
-                                    Stunting juga dipengaruhi aspek perilaku, terutama pada pola asuh yang kurang baik 
-                                    dalam praktek pemberian makan bagi bayi dan Balita.
-
-                                    Dimulai dari edukasi tentang kesehatab reproduksi dan gizi bagi remaja sebagai 
-                                    cikal bakal keluarga, hingga para calon ibu memahami pentingnya memenuhi 
-                                    kebutuhan gizi saat hamil dan stimulasi bagi janin, serta memeriksakan 
-                                    kandungan empat kali selama kehamilan.
-                                </p>
-                            </li>
-                            <li>
-                                <h3>Sanitasi dan Akses Air Bersih</h3>
-                                <p>
-                                    Rendahnya akses terhadap pelayanan kesehatan, 
-                                    termasuk di dalamnya adalah akses sanitasi dan air bersih, mendekatkan anak pada 
-                                    risiko ancaman penyakit infeksi. Untuk itu, perlu membiasakan cuci tangan pakai 
-                                    sabun dan air mengalir, serta tidak buang air besar sembarangan.
-                                </p>
-                            </li>
-                        </ol>
-                        <p>
-                            Sumber: Kementrian Kesehatan Republik Indonesia<br />
-                            <a href="http://p2ptm.kemkes.go.id/kegiatan-p2ptm/subdit-penyakit-diabetes-melitus-dan-gangguan-metabolik/cegah-stunting-dengan-perbaikan-pola-makan-pola-asuh-dan-sanitasi">http://p2ptm.kemkes.go.id/kegiatan-p2ptm/subdit-penyakit-diabetes-melitus-dan-gangguan-metabolik/cegah-stunting-dengan-perbaikan-pola-makan-pola-asuh-dan-sanitasi</a>
-                        </p>
-                    </Grid>
-                    <Grid 
-                        item 
-                        xs={6} 
-                        md={4} 
-                        alignItems="center"
-                    >
-                        <h2>Artikel Lain :</h2>
-                        { dummyArticles.filter(article => article.id != id).map(article => (
-                            <>
-                                <img width={332} src={article.image} />
-                                <h3>
-                                    <Link 
-                                        component={RouterLink} 
-                                        to={`/article/${article.id}`}
-                                        underline="none"
-                                        color="black"
-                                    >
-                                        {article.title}
-                                    </Link>
-                                </h3>
-                            </>
-                        )) }
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    )
+const MenuDetailPage = () => {
+  const dummyData = {
+    title: "Pure Pisang",
+    bahan: [
+      "Pisang Segar dan matang"
+    ],
+    resep: [
+      "Cuci pisang yang belum dikupas dengan campuran air dan cuka untuk menyingkirkan bakteri,dan keringkan.",
+      "Kupas pisang dan potong kecil-kecil.",
+      "Haluskan pisang dengan food processor.",
+      "Campur pisang dengan air atau ASI hingga mencapai konsistensi yang diinginkan."
+    ],
+    saranTitle: "Tips pemberian MPASI untuk bayi 6 bulan",
+    saran: [
+      "Meski bayi sudah diperkenalkan pada makanan padat, pemberian ASI tetap harus berjalan.",
+      "Untuk bayi berusia 6 bulan, berikan MPASI sebanyak 2-3 kali sehari dan 1 sampai 2 kali makanan selingan setiap hari.",
+      "Berikan 2-3 sendok MPASI dalam sekali makan, sebagai awalan.",
+      "Jumlah porsi dapat meningkat perlahan, hingga menjadi setengah mangkuk berukuran 250 ml.",
+      "Berikan makanan secara perlahan.",
+      "Jangan memaksakan bayi untuk menghabiskan makanannya.",
+      "Durasi makan tidak boleh lebih dari 3 menit.",
+      "Jangan memberikan makanan sebagai hadiah.",
+      "Jangan makan sambil menonton televisi atau dari perangkat lainnya.",
+      "Motivasi anak untuk makan sendiri.",
+      "Berikan makanan dalam porsi kecil.",
+      "Hentikan pemberian makan jika dalam 15 menit anak hanya bermain-main tanpa makan."
+    ]
+  }
+  const { id } = useParams<{ id: string }>();
+  return (
+    <>
+      <Header />
+        <Container maxWidth="lg">
+          <h1>Sarapan</h1>
+          <img style={{objectFit: 'cover'}} width={930} height={290} src="https://asset.kompas.com/crops/3swuFc9jPQW51QyLDYsJtJ00YIo=/0x0:780x520/750x500/data/photo/2020/07/22/5f17e48c27e8b.jpg" />
+          <h2>{dummyData.title}</h2>
+          <h3>Bahan-bahan: </h3>
+          <ol>
+            { dummyData.bahan.map(bahan => <li>{bahan}</li>) }
+          </ol>
+          <h3>Resep: </h3><ol>
+            { dummyData.resep.map(resep => <li>{resep}</li>) }
+          </ol>
+          <h3>{dummyData.saranTitle}</h3>
+          <ol>
+            { dummyData.saran.map(saran => <li>{saran}</li>) }
+          </ol>
+        </Container>
+    </>
+  )
 }
 
-export default ArticleDetailPage
+export default MenuDetailPage
